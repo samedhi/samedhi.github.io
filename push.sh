fbff605aae8dfbe0dccb68bd9f7846e50afa5c34
Stephen Cagle <stephen@read-line.com>
2018-01-02 18:05:52 -0800

    Update push.sh file

--- push.sh
+++ push.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-b=$(git branch | grep -e "^*" | cut -d' ' -f 2)
+branch=$(git branch | grep -e "^*" | cut -d' ' -f 2)
 jekyll build
 rm push.sh
 git checkout master
 cp -r _site/* .
+git add .
+git commit -m "AUTO COMMIT"
+git push
+git checkout $branch
